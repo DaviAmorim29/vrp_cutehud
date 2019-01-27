9b89950b7d3e99c57cad743ee850040fc328d55b
Amorim <daviamorim29@hotmail.com>
2019-01-27 04:18:00 -0200

    Update client.lua

--- vrp/client.lua
+++ vrp/client.lua
@@ -1,1 +1,21 @@
+local thirst = 0
+local hunger = 0
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(100)
+        TriggerServerEvent("getBasics")
+        SendNUIMessage({
+            show = IsPauseMenuActive(),
+            armor = GetPedArmour(GetPlayerPed(-1)),
+            health = (GetEntityHealth(GetPlayerPed(-1))-100),
+            hunger = hunger,
+            thirst = thirst
+        })
+    end
+end)
 
+RegisterNetEvent("returnBasics")
+AddEventHandler("returnBasics", function (rHunger, rThirst)
+    hunger = rHunger
+    thirst = rThirst
+end)
